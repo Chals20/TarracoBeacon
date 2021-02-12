1be84f31312f98b71e8b05ea155ef436a63e03b8
Jesus Lopez <jesuslp.06@gmail.com>
2021-02-12 05:35:52 +0100

    Beacon Jesús = Balcó del Mediterrani

--- app/src/main/java/com/example/demobeacon/NotificationActivityJesus.java
+++ app/src/main/java/com/example/demobeacon/NotificationActivityJesus.java
@@ -16,11 +16,12 @@ public class NotificationActivityJesus extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notification);
         ImageView image = findViewById(R.id.imageView);
-        Picasso.get().load("https://s2.wklcdn.com/image_72/2188616/22555187/14357633Master.jpg").resize(1000,562).into(image);
+        Picasso.get().load("https://www.tarragonaturisme.cat/sites/default/files/styles/full_image_with_copyright/public/monument/gallery/25_-_ciutat_-_balco_del_mediterrani_amb_gent.jpg?itok=jaxtc2d1").resize(1000,562).into(image);
         TextView title = findViewById(R.id.title);
-        title.setText("Beacon de Jesus");
+        title.setText("Balcó del Mediterrani");
         TextView description = findViewById(R.id.description);
-        description.setText("Este es el beacon de Jesus");
+        description.setText("El Balcó del Mediterrani es trobat situat al capdamunt de la Rambla Nova, a uns 40 metres sobre el mar, permeten una visió privilegiada del Mare Nostrum, el port de Tarragona, la platja del Miracle i l'Amfiteatre. +" +
+                "Lloc molt estimat pels tarragonins i les tarragonines, on conflueixen tots els qui, passejant, van 'a tocar ferro'. La barana té una forma inconfusible i es diu que tocar-la dóna sort.");
 
     }
 }
