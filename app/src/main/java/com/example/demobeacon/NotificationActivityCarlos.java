6d3c63b763848f745760adbedad9566f64b5ee0d
Chals <csanchezbasket@gmail.com>
2021-02-12 01:05:39 +0100

    Commit Beacon Carlos

--- app/src/main/java/com/example/demobeacon/NotificationActivityCarlos.java
+++ app/src/main/java/com/example/demobeacon/NotificationActivityCarlos.java
@@ -19,9 +19,10 @@ public class NotificationActivityCarlos extends Activity {
         ImageView image = findViewById(R.id.imageView);
         Picasso.get().load("https://s2.wklcdn.com/image_72/2188616/22555187/14357633Master.jpg").resize(1000,562).into(image);
         TextView title = findViewById(R.id.title);
-        title.setText("Beacon de Carlos");
+        title.setText("Circ Romà");
         TextView description = findViewById(R.id.description);
-        description.setText("Este es el beacon de Carlos");
+        description.setText("El circ de Tarraco es trobava emplaçat dins el pomerium de la ciutat. +" +
+                "Situat a la part alta de Tarragona, manté un estat excepcional de conservació.");
 
     }
 
